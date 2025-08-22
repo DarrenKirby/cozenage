307176aaaa8e0ffafb9e21ec3201818c90937c47
Darren Kirby <bulliver@gmail.com>
2025-08-22 15:01:06 -0700

    Initial commit of environment.h/.c

---
+++ src/environment.c
@@ -0,0 +1,5 @@
+//
+// Created by Darren Kirby on 2025-08-22.
+//
+
+#include "environment.h"
---
+++ src/environment.h
@@ -0,0 +1,8 @@
+//
+// Created by Darren Kirby on 2025-08-22.
+//
+
+#ifndef COZENAGE_ENVIRONMENT_H
+#define COZENAGE_ENVIRONMENT_H
+
+#endif //COZENAGE_ENVIRONMENT_H
