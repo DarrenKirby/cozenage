57a4241097b0447ae161062cf5bb1af19b84b4b8
Darren Kirby <bulliver@gmail.com>
2025-08-24 23:56:43 -0700

    Add print support for pairs and lists

--- src/printer.c
+++ src/printer.c
@@ -8,7 +8,7 @@
 #include "main.h"
 
 
-void print_lval_list(const l_val* v, const char* prefix, const char open, const char close) {
+void print_lval_seq(const l_val* v, const char* prefix, const char open, const char close) {
     if (!v) return;
 
     if (prefix) printf("%s", prefix);
@@ -22,67 +22,92 @@ void print_lval_list(const l_val* v, const char* prefix, const char open, const 
     printf("%c", close);
 }
 
+void print_pair(const l_val* v) {
+    printf("(");
+    const l_val* cur = v;
+    while (cur->type == LVAL_PAIR) {
+        print_lval(cur->car);
+        if (cur->cdr->type == LVAL_NIL) {
+            break;  // proper end of list
+        } else if (cur->cdr->type == LVAL_PAIR) {
+            printf(" ");
+            cur = cur->cdr;
+            continue;
+        } else {
+            printf(" . ");
+            print_lval(cur->cdr);
+            break;
+        }
+    }
+    printf(")");
+}
+
+
 void print_lval(const l_val* v) {
     switch (v->type) {
-        case LVAL_FLOAT:
-            printf("%Lg", v->float_n);
-            break;
+    case LVAL_FLOAT:
+        printf("%Lg", v->float_n);
+        break;
 
-        case LVAL_INT:
-            printf("%lld", v->int_n);
-            break;
+    case LVAL_INT:
+        printf("%lld", v->int_n);
+        break;
 
-        case LVAL_BOOL:
-            printf("%s%s%s", ANSI_MAGENTA,
-                   v->boolean ? "#t" : "#f",
-                   ANSI_RESET);
-            break;
+    case LVAL_BOOL:
+        printf("%s%s%s", ANSI_MAGENTA,
+               v->boolean ? "#t" : "#f",
+               ANSI_RESET);
+        break;
 
-        case LVAL_ERR:
-            printf("%sError:%s %s", ANSI_RED_B, ANSI_RESET, v->str);
-            break;
+    case LVAL_ERR:
+        printf("%sError:%s %s", ANSI_RED_B, ANSI_RESET, v->str);
+        break;
 
-        case LVAL_CHAR:
-            switch (v->char_val) {
-            case '\n': printf("#\\newline"); break;
-            case ' ':  printf("#\\space");   break;
-            case '\t': printf("#\\tab");     break;
-            default:   printf("#\\%c", v->char_val); break;
-            }
-            break;
+    case LVAL_CHAR:
+        switch (v->char_val) {
+        case '\n': printf("#\\newline"); break;
+        case ' ':  printf("#\\space");   break;
+        case '\t': printf("#\\tab");     break;
+        default:   printf("#\\%c", v->char_val); break;
+        }
+        break;
 
-        case LVAL_STR:
-            printf("\"%s\"", v->str);
-            break;
+    case LVAL_STR:
+        printf("\"%s\"", v->str);
+        break;
 
-        case LVAL_FUN:
-            if (v->name) {
-                printf("<builtin procedure '%s%s%s'>", ANSI_GREEN_B, v->name, ANSI_RESET);
-            } else {
-                printf("<lambda>");
-            }
-            break;
+    case LVAL_FUN:
+        if (v->name) {
+            printf("<builtin procedure '%s%s%s'>", ANSI_GREEN_B, v->name, ANSI_RESET);
+        } else {
+            printf("<lambda>");
+        }
+        break;
 
-        case LVAL_SYM:
-            printf("%s", v->sym);
-            break;
+    case LVAL_SYM:
+        printf("%s", v->sym);
+        break;
 
-        case LVAL_NIL:
-            printf("()");
+    case LVAL_PAIR:
+        print_pair(v);
             break;
-        case LVAL_SEXPR:
-            print_lval_list(v, NULL, '(', ')');
-            break;
-        case LVAL_VECT:
-            print_lval_list(v, "#", '(', ')');
-            break;
-        case LVAL_BYTEVEC:
-            print_lval_list(v, "#u8", '(', ')');
-            break;
+    case LVAL_NIL:
+        printf("()");
+        break;
 
-        default:
-            printf("%sError:%s unknown type: '%s%d%s'", ANSI_RED_B,
-                ANSI_RESET, ANSI_RED_B, v->type, ANSI_RESET);
+    case LVAL_SEXPR:
+        print_lval_seq(v, NULL, '(', ')');
+        break;
+    case LVAL_VECT:
+        print_lval_seq(v, "#", '(', ')');
+        break;
+    case LVAL_BYTEVEC:
+        print_lval_seq(v, "#u8", '(', ')');
+        break;
+
+    default:
+        printf("%sError:%s unknown type: '%s%d%s'", ANSI_RED_B,
+            ANSI_RESET, ANSI_RED_B, v->type, ANSI_RESET);
     }
 }
 
