93953811f1d7aff4a8309823d0cbd41807ae51a0
Darren Kirby <bulliver@gmail.com>
2025-08-31 18:41:26 -0700

    Add win32 macro test; define fake readline functions

--- src/compat_readline.h
+++ src/compat_readline.h
@@ -2,6 +2,27 @@
 #ifndef COZENAGE_COMPAT_READLINE_H
 #define COZENAGE_COMPAT_READLINE_H
 
+
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+#else
+
 #include <stdio.h>
 
 #if __has_include(<readline/readline.h>)
@@ -11,6 +32,8 @@
 #  include <editline/readline.h>
 #else
 #  error "No readline-compatible headers found"
+#endif
+
 #endif
 
 #endif //COZENAGE_COMPAT_READLINE_H
