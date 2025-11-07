cbe33d3113c4d49bf3156b40fa0d19caafcbc987
Darren Kirby <bulliver@gmail.com>
2025-11-06 20:56:07 -0800

    New file for error procedures

---
+++ src/errors.c
@@ -0,0 +1,35 @@
+/*
+ * 'errors.c'
+ * This file is part of Cozenage - https://github.com/DarrenKirby/cozenage
+ * Copyright © 2025  Darren Kirby <darren@dragonbyte.ca>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "errors.h"
+#include "cell.h"
+
+#include "types.h"
+
+Cell* builtin_error_object(const Lex* e, const Cell* a)
+{
+    (void)e;
+    Cell* err = CHECK_ARITY_EXACT(a, 1);
+    if (err) return err;
+
+    if (a->cell[0]->type == CELL_ERROR) {
+        return make_cell_boolean(1);
+    }
+    return make_cell_boolean(0);
+}
