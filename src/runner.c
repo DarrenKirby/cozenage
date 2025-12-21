e11dd8611ba6f68a153ae514ec220a17fcedb6c3
Darren Kirby <bulliver@gmail.com>
2025-12-21 10:38:22 -0800

    Comment out transform debug prints

--- src/runner.c
+++ src/runner.c
@@ -153,10 +153,17 @@ Cell* parse_all_expressions(Lex* e, TokenArray* ta, const bool is_repl)
             break;
         }
 
+        /* For transform debugging */
+        //printf("Before transform: \n");
+        //debug_print_cell(expression);
         /* Kick all S-expressions off to the transformer. */
         if (expression->type == CELL_SEXPR) {
             expression = expand(expression);
         }
+
+        /* For transform debugging */
+        //printf("After transform: \n");
+        //debug_print_cell(expression);
 
         /* Raise error if generated in parsing or transforming. */
         if (expression->type == CELL_ERROR) {
