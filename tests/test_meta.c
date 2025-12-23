be559657d4ea2ea6b0c89f5f5561e66508b7e9bd
Darren Kirby <bulliver@gmail.com>
2025-12-22 17:35:15 -0800

    Add more tests for transforms and special forms

--- tests/test_meta.c
+++ tests/test_meta.c
@@ -4,7 +4,9 @@
 #include "../src/parser.h"
 #include "../src/repr.h"
 #include "../src/symbols.h"
+#include "../src/transforms.h"
 #include <locale.h>
+#include <gc/gc.h>
 
 /* Define the global test environment variable. */
 Lex* test_env;
@@ -30,26 +32,27 @@ void teardown_each_test(void) {
     //ht_destroy(symbol_table);
 }
 
-/* I moved all the setup logic here because there
- * was some weird Criterion - libgc interaction
- * that was causing bizarre memory corruption,
- * and causing tests to fail when they should not.
- *
- * Yes - the tests will run slower, but tests aren't
- * supposed to be fast,they're supposed to test if
- * the code works!
- */
+static bool engine_prepped = false;
+
 char* t_eval(const char* input) {
-    symbol_table = ht_create(128);
+    if (!engine_prepped) {
+        GC_INIT(); // Only call this ONCE per process
+        symbol_table = ht_create(128);
+        init_global_singletons();
+        init_special_forms();
+        engine_prepped = true;
+    }
+
+    // These ARE safe to refresh per call
     init_default_ports();
-    init_global_singletons();
     test_env = lex_initialize_global_env();
     lex_add_builtins(test_env);
-    init_special_forms();
 
     TokenArray* ta = scan_all_tokens(input);
     Cell* parsed = parse_tokens(ta);
-    const Cell *result = coz_eval(test_env, parsed);
+    Cell* expr = expand(parsed);
+    const Cell *result = coz_eval(test_env, expr);
 
     return cell_to_string(result, MODE_WRITE);
 }
+
--- tests/test_special_forms.c
+++ tests/test_special_forms.c
@@ -16,6 +16,58 @@ Test(end_to_end_sf, test_lambda, .init = setup_each_test, .fini = teardown_each_
     cr_assert_str_eq(t_eval("((lambda (x y) (+ x y)) 2 3)"), "5");
 }
 
+Test(end_to_end_sf, test_boolean_logic, .init = setup_each_test, .fini = teardown_each_test) {
+    // and/or short-circuiting and truthiness
+    cr_assert_str_eq(t_eval("(and 1 2 3)"), "3"); // returns last value
+    cr_assert_str_eq(t_eval("(and #t #f (define x 1))"), "#false");
+    cr_assert_str_eq(t_eval("(or #f 5 #t)"), "5"); // returns first truthy value
+
+    // when/unless
+    cr_assert_str_eq(t_eval("(when #t 1 2 3)"), "3");
+    cr_assert_str_eq(t_eval("(unless #f 1 2 3)"), "3");
+    cr_assert_str_eq(t_eval("(when #f 1)"), "");
+}
+
+Test(end_to_end_sf, test_bindings, .init = setup_each_test, .fini = teardown_each_test) {
+    // let*: variables can see previous variables in same block
+    cr_assert_str_eq(t_eval("(let* ((x 1) (y (+ x 1))) y)"), "2");
+
+    // letrec: mutual recursion
+    cr_assert_str_eq(t_eval(
+        "(letrec ((is-even? (lambda (n) (if (= n 0) #t (is-odd? (- n 1))))) "
+        "         (is-odd?  (lambda (n) (if (= n 0) #f (is-even? (- n 1)))))) "
+        "(is-even? 4))"), "#true");
+
+    // Named let (your new transform!)
+    cr_assert_str_eq(t_eval("(let loop ((i 3)) (if (= i 0) 'done (loop (- i 1))))"), "done");
+}
+
+Test(end_to_end_sf, test_conditionals, .init = setup_each_test, .fini = teardown_each_test) {
+    // cond
+    cr_assert_str_eq(t_eval("(cond (#f 1) ((= 1 2) 2) (else 3))"), "3");
+    cr_assert_str_eq(t_eval("(cond ((+ 1 1) 'truthy))"), "truthy");
+
+    // case (your transform!)
+    cr_assert_str_eq(t_eval("(case (* 2 3) ((2 3 5) 'prime) ((6 8 10) 'even) (else 'idk))"), "even");
+    cr_assert_str_eq(t_eval("(case 'apple ((banana) 1) ((orange) 2) (else 3))"), "3");
+}
+
+Test(end_to_end_sf, test_state, .init = setup_each_test, .fini = teardown_each_test) {
+    cr_assert_str_eq(t_eval("(begin (define x 10) (set! x 20) x)"), "20");
+    // Ensure set! works on closure variables
+    cr_assert_str_eq(t_eval(
+        "(begin "
+        "  (define counter ((lambda (n) (lambda () (set! n (+ n 1)) n)) 0)) "
+        "  (counter) (counter))"), "2");
+}
+
+Test(end_to_end_sf, test_iteration, .init = setup_each_test, .fini = teardown_each_test) {
+    // Sum numbers 1 to 5
+    cr_assert_str_eq(t_eval(
+        "(do ((i 1 (+ i 1)) (sum 0 (+ sum i))) "
+        "    ((> i 5) sum))"), "15");
+}
+
 //Test(end_to_end_sf, test_map_procedure, .init = setup_each_test, .fini = teardown_each_test) {}
 
 //Test(end_to_end_sf, test_map_procedure, .init = setup_each_test, .fini = teardown_each_test) {}
