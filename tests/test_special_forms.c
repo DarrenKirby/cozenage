77229765a16337f1f74dde96ffae0c15dde59587
Darren Kirby <bulliver@gmail.com>
2025-12-29 09:42:06 -0800

    Add more tests for special forms, including libgc stress-test

--- tests/test_special_forms.c
+++ tests/test_special_forms.c
@@ -1,6 +1,7 @@
 #include <criterion/criterion.h>
 
 #include "test_meta.h"
+#include <gc/gc.h>
 
 TestSuite(end_to_end_sf);
 
@@ -38,7 +39,7 @@ Test(end_to_end_sf, test_bindings, .init = setup_each_test, .fini = teardown_eac
         "         (is-odd?  (lambda (n) (if (= n 0) #f (is-even? (- n 1)))))) "
         "(is-even? 4))"), "#true");
 
-    // Named let (your new transform!)
+    // Named let
     cr_assert_str_eq(t_eval("(let loop ((i 3)) (if (= i 0) 'done (loop (- i 1))))"), "done");
 }
 
@@ -47,7 +48,7 @@ Test(end_to_end_sf, test_conditionals, .init = setup_each_test, .fini = teardown
     cr_assert_str_eq(t_eval("(cond (#f 1) ((= 1 2) 2) (else 3))"), "3");
     cr_assert_str_eq(t_eval("(cond ((+ 1 1) 'truthy))"), "truthy");
 
-    // case (your transform!)
+    // case
     cr_assert_str_eq(t_eval("(case (* 2 3) ((2 3 5) 'prime) ((6 8 10) 'even) (else 'idk))"), "even");
     cr_assert_str_eq(t_eval("(case 'apple ((banana) 1) ((orange) 2) (else 3))"), "3");
 }
@@ -66,6 +67,85 @@ Test(end_to_end_sf, test_iteration, .init = setup_each_test, .fini = teardown_ea
     cr_assert_str_eq(t_eval(
         "(do ((i 1 (+ i 1)) (sum 0 (+ sum i))) "
         "    ((> i 5) sum))"), "15");
+}
+
+Test(end_to_end_sf, test_conditionals_exhaustive, .init = setup_each_test, .fini = teardown_each_test) {
+    // cond: arrow form
+    cr_assert_str_eq(t_eval("(cond ((assoc 'b '((a . 1) (b . 2))) => cdr))"), "2");
+
+    // cond: test-only (should return the result of the test)
+    cr_assert_str_eq(t_eval("(cond ((member 2 '(1 2 3))))"), "(2 3)");
+
+    // cond: implicit begin in clauses
+    cr_assert_str_eq(t_eval("(cond (#t (define a 1) (define b 2) (+ a b)))"), "3");
+
+    // case: atom vs list of keys
+    cr_assert_str_eq(t_eval("(case 'a ((b c) 1) ((a) 2))"), "2");
+
+    // case: with expressions as the key
+    cr_assert_str_eq(t_eval("(case (+ 1 1) ((2) 'yes) (else 'no))"), "yes");
+}
+
+Test(end_to_end_sf, test_logic_exhaustive, .init = setup_each_test, .fini = teardown_each_test) {
+    // and: returns last value if all true
+    cr_assert_str_eq(t_eval("(and 1 'a \"ok\")"), "\"ok\"");
+    // and: short-circuit (should not reach the error)
+    cr_assert_str_eq(t_eval("(and #f (/ 1 0))"), "#false");
+    // and: empty
+    cr_assert_str_eq(t_eval("(and)"), "#true");
+
+    // or: returns first truthy value
+    cr_assert_str_eq(t_eval("(or #f 0 #f)"), "0"); // 0 is truthy in Scheme!
+    // or: short-circuit
+    cr_assert_str_eq(t_eval("(or 1 (/ 1 0))"), "1");
+    // or: empty
+    cr_assert_str_eq(t_eval("(or)"), "#false");
+}
+
+Test(end_to_end_sf, test_bindings_exhaustive, .init = setup_each_test, .fini = teardown_each_test) {
+    // letrec*: sequential recursive initialization
+    // In letrec, this might fail. In letrec*, 'y' must see 'x' as 10.
+    cr_assert_str_eq(t_eval("(letrec* ((x 10) (y (+ x 5))) y)"), "15");
+
+    // Nested Named Let (Shadowing check)
+    cr_assert_str_eq(t_eval(
+        "(let loop ((x 5)) "
+        "  (if (= x 0) "
+        "      'done "
+        "      (let ((x 100)) (loop 0))))"), "done");
+
+    // let: check that bindings are simultaneous (x should refer to outer x)
+    cr_assert_str_eq(t_eval("(begin (define x 1) (let ((x 2) (y x)) y))"), "1");
+}
+
+Test(end_to_end_sf, test_internal_defines, .init = setup_each_test, .fini = teardown_each_test) {
+    // Internal define in when
+    cr_assert_str_eq(t_eval("(when #t (define a 10) (define b 20) (+ a b))"), "30");
+
+    // Internal define in lambda
+    cr_assert_str_eq(t_eval("((lambda (x) (define y 10) (+ x y)) 5)"), "15");
+
+    // Internal define in unless
+    cr_assert_str_eq(t_eval("(unless #f (define z \"ok\") z)"), "\"ok\"");
+}
+
+Test(end_to_end_sf, test_gc_stress, .init = setup_each_test, .fini = teardown_each_test) {
+    GC_gcollect(); // Force a collection before we start
+    size_t heap_before = GC_get_heap_size();
+
+    // 1 Million iterations of a do-loop transform
+    const char* result = t_eval(
+        "(do ((i 0 (+ i 1)) (sum 0 (+ sum i))) "
+        "    ((>= i 1000000) \"Done\"))");
+
+    GC_gcollect(); // Force a collection after we finish
+    size_t heap_after = GC_get_heap_size();
+
+    printf("\n[GC Stats] Heap Before: %zu bytes\n", heap_before);
+    printf("[GC Stats] Heap After:  %zu bytes\n", heap_after);
+    printf("[GC Stats] Growth:      %zd bytes\n", (ssize_t)heap_after - (ssize_t)heap_before);
+
+    cr_assert_str_eq(result, "\"Done\"");
 }
 
 //Test(end_to_end_sf, test_map_procedure, .init = setup_each_test, .fini = teardown_each_test) {}
