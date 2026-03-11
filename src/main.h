6aad26281a84b4840d816a82400a6bbb10b79128
Darren Kirby <darren@dragonbyte.ca>
2026-03-11 09:58:59 -0700

    Version bump

--- src/main.h
+++ src/main.h
@@ -24,7 +24,7 @@
 
 /* Note: version string typically provided by CMake. */
 #ifndef APP_VERSION
-#define APP_VERSION "0.10.0"
+#define APP_VERSION "0.16.0"
 #endif
 
 #define PS1_PROMPT "--> \001\x1b[37;1m\002"
